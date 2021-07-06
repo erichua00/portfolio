41a3c21c346ef80a2b0ec95251a137d47ce2870d
Eric Chua <ericcyh130259@gmail.com>
2021-07-06 17:32:22 +0800

    refine

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,22 @@ def allblogs(request):
 def detail(request, blog_id):
     detailblog=get_object_or_404(Blog, pk=blog_id)
     return render(request, 'blog/detail.html', {'blog':detailblog})
+
+def win10optimize(request):
+    return render(request, 'blog/win10optimize.html')
+
+def step_1(request):
+        return render(request, 'blog/step_1.html')
+"""
+def step_2(request):
+        return render(request, 'blog/step_2.html')
+
+def step_3(request):
+        return render(request, 'blog/step_3.html')
+
+def step_4(request):
+        return render(request, 'blog/step_4.html')
+
+def step_5(request):
+        return render(request, 'blog/step_5.html')
+"""
