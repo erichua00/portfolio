84e34cc0db8f53a76371594eeabcd43f2cea6a36
Eric Chua <ericcyh130259@gmail.com>
2021-07-08 22:05:12 +0800

    new

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
-"""
-def allblogs(request):
+def blogs(request):
     blogs=Blog.objects
-    return render(request, 'blog/allblogs.html',{'blogs':blogs})
+    return render(request, 'blog/blogs.html',{'blogs':blogs})
 
 def detail(request, blog_id):
-    detailblog=get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'blog/detail.html', {'blog':detailblog})
-"""
-
-def win10optimize(request):
-    return render(request, 'blog/win10optimize.html')
-
-def step_1(request):
-        return render(request, 'blog/step_1.html')
-"""
-def step_2(request):
-        return render(request, 'blog/step_2.html')
-
-def step_3(request):
-        return render(request, 'blog/step_3.html')
-
-def step_4(request):
-        return render(request, 'blog/step_4.html')
-
-def step_5(request):
-        return render(request, 'blog/step_5.html')
-"""
+    detail=get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'detail':detail})
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -9,11 +9,5 @@ import blog.views
 urlpatterns = [
     path('erichua/secure/secure/secure/admin', admin.site.urls),
     path("",jobs.views.home,name='home'),
-    #path("blog/", include('blog.urls')),
-    path("win10optimize/", blog.views.win10optimize, name='win10optimize'),
-    path("win10optimize/step_1", blog.views.step_1, name='step_1'),
-    #path("win10optimize/step_2", blog.views.step_2, name='step_2'),
-    #path("win10optimize/step_3", blog.views.step_3, name='step_3'),
-    #path("win10optimize/step_4", blog.views.step_4, name='step_4'),
-    #path("win10optimize/step_5", blog.views.step_5, name='step_5'),
+    path("blog/", include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
